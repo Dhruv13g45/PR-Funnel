73e878b267a95f8c588fe52c73302c62d4a8db74
Dhruv Goradia <dhruvhgoradia@gmail.com>
2026-08-26 16:54:06 +0530

    test: trigger PR webhook

--- server/src/services/github.services.ts
+++ server/src/services/github.services.ts
@@ -5,22 +5,18 @@ export async function githubWebhookService(event: string, payload: any) {
   switch (event) {
     case "installation":
       console.log("Installation Event");
-      console.log(payload);
       break;
 
     case "installation_repositories":
       console.log("Installation Repositories Event");
-      console.log(payload);
       break;
 
     case "pull_request":
       console.log("Pull Request Event");
-      console.log(payload);
       break;
 
     case "ping":
       console.log("Ping Event");
-      console.log(payload);
       break;
 
     default:
