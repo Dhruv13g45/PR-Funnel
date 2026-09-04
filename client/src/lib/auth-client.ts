3b0de085f0a38220d8b4043c5a3e3b6c97051383
Dhruv Goradia <dhruvhgoradia@gmail.com>
2026-09-04 14:07:43 +0530

    fixing bugs

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
+  // baseURL: import.meta.env.VITE_BACKEND_URL!,
   baseURL: import.meta.env.VITE_BACKEND_URL!,
 });
